a9ffe2f9525ed17fd55f9a554491118ec1cba59b
Amey <ameydeshingkar@gmail.com>
2022-07-18 11:54:01 +0530

    Print Subsequence

--- printSubsequence.py
+++ printSubsequence.py
@@ -5,8 +5,10 @@ def printSubsequence(ind, ds, arr, n):
     if ind >= n:
         print(ds)
         return
+    # Take Condition
     ds.append(arr[ind])
     printSubsequence(ind+1, ds, arr, n)
+    # Remove Condition
     ds.pop()
     printSubsequence(ind+1, ds, arr, n)
 
